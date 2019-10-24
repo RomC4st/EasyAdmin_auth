e0d6ca8b83114063ada9bd61ed78edbbfe73c023
Romain Castillon <castillonromain01@gmail.com>
2019-10-24 15:19:10 +0200

    add mapbox

---
+++ public/assets/js/app.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3IwbjBzIiwiYSI6ImNqdHB1cTQ2azA3cmw0M2swdGtiY3Noa3MifQ.EM0c8d_0JEcc3FOLQ8P0CA';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [2.3488, 48.8534],
+  zoom: 12,
+});
+
+
+
+

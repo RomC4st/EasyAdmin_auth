787335ed72d01cf4a6b17b853c50d05d970d503f
Romain Castillon <castillonromain01@gmail.com>
2019-10-24 23:13:35 +0200

    enable user modification with apiplatform

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,10 +5,8 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-use ApiPlatform\Core\Annotation\ApiResource;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ApiResource
  */
 class User implements UserInterface
 {

18dfc2f0a928588891ada3dd0fe1e13180aa26c0
Romain Castillon <castillonromain01@gmail.com>
2019-10-27 13:44:46 +0100

    update ProductFixtures

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -17,8 +17,8 @@ class ProductFixtures extends Fixture
         $product1->setName('Bois');
         $product1 ->setNumber(5);
         $product1-> setIsActive(true);
-        $product1-> setLatitude(48.844981);
-        $product1-> setLongitude(2.314107);
+        $product1-> setLatitude(48.8265663);
+        $product1-> setLongitude(2.3403299);
         $product1-> setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, 
         enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. ');
         $product1-> setAdress("3 rue d'alésia 75014 Paris");
@@ -28,8 +28,8 @@ class ProductFixtures extends Fixture
         $product2->setName('Verre');
         $product2 ->setNumber(2);
         $product2-> setIsActive(true);
-        $product2-> setLatitude(48.856729);
-        $product2-> setLongitude(2.320973);
+        $product2-> setLatitude(48.8287693);
+        $product2-> setLongitude(2.3146889);
         $product2-> setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, 
         enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. ');
         $product2-> setAdress("18 rue pierre larousse 75014 Paris");
@@ -39,8 +39,8 @@ class ProductFixtures extends Fixture
         $product3->setName('Metal');
         $product3 ->setNumber(10);
         $product3-> setIsActive(true);
-        $product3-> setLatitude(48.872087);
-        $product3-> setLongitude(2.337453);
+        $product3-> setLatitude(48.8323603);
+        $product3-> setLongitude(2.3223177);
         $product3-> setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, 
         enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. ');
         $product3-> setAdress("1 rue de l'eure 75014 Paris");

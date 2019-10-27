1559c1c5637b8ce7f37ce36a98019833c06de3df
Romain Castillon <castillonromain01@gmail.com>
2019-10-27 17:38:39 +0100

    try catch statement in service

--- src/Service/GetCoordinates.php
+++ src/Service/GetCoordinates.php
@@ -10,17 +10,24 @@ class GetCoordinates
 {
     public function indexAction($addr)
     {
-      $geocoder = new \Geocoder\ProviderAggregator();
-      $adapter  = new \Http\Adapter\Guzzle6\Client();
-      $apiKey=$_ENV['API_LOCATIONIQ_KEY'];
+      try {
+        $geocoder = new \Geocoder\ProviderAggregator();
+        $adapter  = new \Http\Adapter\Guzzle6\Client();
+        $apiKey=$_ENV['API_LOCATIONIQ_KEY'];
         $geocoder->registerProviders([
-          new \Geocoder\Provider\LocationIQ\LocationIQ($adapter,$apiKey)
-      ]);   
-      $result = $geocoder->geocodeQuery(GeocodeQuery::create($addr));
-      $coordinates= $result->get(0)->getCoordinates();
-      $latitude = $coordinates->getLatitude(); 
-      $longitude = $coordinates->getLongitude();
-      $geoloc= array('latitude'=>$latitude,'longitude'=>$longitude);
+            new \Geocoder\Provider\LocationIQ\LocationIQ($adapter,$apiKey)
+        ]);   
+
+        $result = $geocoder->geocodeQuery(GeocodeQuery::create($addr));
+        $coordinates= $result->get(0)->getCoordinates();
+        $latitude = $coordinates->getLatitude(); 
+        $longitude = $coordinates->getLongitude();
+
+        $geoloc= array('latitude'=>$latitude,'longitude'=>$longitude);
         return $geoloc;
+      }catch(Exception $e){
+        throw new Exception($e->getMessage());
+      }
+     
     }
 }
